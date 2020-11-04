f0c9b87c75d9478afb01c6600f7faa9f4654148b
sikorskr <00209032@pw.edu.pl>
2020-11-04 14:36:47 +0100

    nowe pliki

--- lmp5/insort.c
+++ lmp5/insort.c
@@ -6,8 +6,8 @@ insort (double v[], int n)
   int i, j;
   for (i = 1; i < n; i++) {
     double tmp = v[j];
-    for (j = i-1; v[j] > tmp, j--)
+    for (j = i-1; v[j] > tmp; j--)
       v[j+1] = v[j];
-    v[j+1] = tnp;
+    v[j+1] = tmp;
   }
 }
